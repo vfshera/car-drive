c29237a2210b1d5aee50414bc6ef1c9225772068
Franklin Shera <fshera96@gmail.com>
2021-10-21 07:23:34 +0300

    SOCIAL LOGIN PASSWORD MAIL

---
+++ app/Mail/SocialPasswordMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SocialPasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+
+    public $name;
+    public $pass;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     * 
+     * 
+     */
+    public function __construct($name , $pass)
+    {
+        $this->name = $name;
+        $this->pass = $pass;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('noreply@cardrive.co.ke', env('APP_NAME'))
+        ->subject('Login Credentials '.env('APP_NAME'))
+       ->view('emails.social-password');
+    }
+    
+}
